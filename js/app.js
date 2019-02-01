0d2df44ce5abda7aed3a4e88207634298eb3385f
Bahersaad676 <bahersaad676@gmail.com>
2019-02-01 14:13:02 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,26 +1,20 @@
+// variables
 let fas = [...document.getElementsByClassName('fas')],
 list = [...document.getElementsByClassName("list")],
 cards = [...document.getElementsByClassName("cards")],
-opend = [...document.getElementsByClassName("add")],
+opend = [...document.getElementsByClassName("open")],
 rating = [...document.getElementsByClassName("rating")],
 numMistakes = [],
 stopTimer,
 numCardOpend = [];
 
-
-
-// compare between cards
-
-for (var i = 0; i < list.length; i++) {
+(function mainFunc() {
+  for (var i = 0; i < list.length; i++) {
 
   list[i].onclick = function addToArray() {
     numCardOpend.push(this);
-    this.children[0].classList.add("add");
-
-    if (numCardOpend.length === 2) {
-      if (numCardOpend[0].innerHTML === numCardOpend[1].innerHTML) theSameCard();
-      else if (numCardOpend[0].innerHTML !== numCardOpend[1].innerHTML) notSameCard();
-    }
+    this.children[0].classList.add("open");
+    if (numCardOpend.length === 2) compare();
     if ((opend.length * 2) === list.length) {
         clearInterval(myTimer);
         if (numMistakes.length <= 3) {
@@ -38,9 +32,11 @@ for (var i = 0; i < list.length; i++) {
   }
 
 }
+})();
 
 
-// Timer 
+
+// Timer
 var myTimer;
 (function clock() {
   myTimer = setInterval(myClock, 1000);
@@ -52,20 +48,47 @@ var myTimer;
 
 })();
 
-// if they are the same card
-function theSameCard() {
-  numCardOpend[0].children[0].classList.add("add");
-  numCardOpend[1].children[0].classList.add("add");
-  opend.push(this);   
-  numCardOpend.length = 0;
+// shuffle
+// these two function will shuffle(mix) between cards
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
 }
 
-// if they are not the same card
-function notSameCard() {
+function randomCards() {
+  shuffle(list);
+   cards[0].innerHTML = "";
+   list.forEach(function (item) {
+     cards[0].appendChild(item);
+   });
+   
+}
+
+randomCards();
+
+// compare 
+// this function will compare between cards
+function compare() {
+  // do this if the same card
+  if (numCardOpend[0].innerHTML === numCardOpend[1].innerHTML) {
+  numCardOpend[0].children[0].classList.add("open", "backcolor");
+  numCardOpend[1].children[0].classList.add("open", "backcolor");
+  opend.push(this);   
+  numCardOpend.length = 0;
+  // do this if not the same card
+  } else if (numCardOpend[0].innerHTML !== numCardOpend[1].innerHTML) {
   numMistakes.push(this);
   setTimeout(function () {
-    numCardOpend[0].children[0].classList.remove("add");
-    numCardOpend[1].children[0].classList.remove("add");
+    numCardOpend[0].children[0].classList.remove("open");
+    numCardOpend[1].children[0].classList.remove("open");
     numCardOpend.length = 0;
   }, 600);
+  }
 }
